9a63f54e8fb6efef7e8b46d7971b43baf29fec4d
SaifJava575 <sksaifuddin575@gmail.com>
2023-05-13 16:14:53 +0530

    Spring Boot JWT

--- src/main/java/com/nit/saif/rest/ContactRestController.java
+++ src/main/java/com/nit/saif/rest/ContactRestController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,60 +22,61 @@ import com.nit.saif.entity.Contact;
 import com.nit.saif.exception.ContactNotFoundException;
 import com.nit.saif.service.IContactService;
 import com.nit.saif.validator.ContactValidator;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api")
-@CrossOrigin
+@RequestMapping("/api/info")
 public class ContactRestController {
 	@Autowired
 	private IContactService service;
 	@Autowired
 	private ContactValidator validator;
 
-	
 	@PostMapping("/contact")
-	public ResponseEntity<?> saveContact(@Valid @RequestBody Contact contact){
-		ResponseEntity<?> response =null;
-		Map<String,String> errors=validator.validation(contact);
-			if(errors.isEmpty()) {
-				String message=service.upsert(contact);
-				response=ResponseEntity.ok(message);	
-			} else {
-				response = new ResponseEntity<Map<String,String>>(
-						errors,HttpStatus.BAD_REQUEST);
-			}		
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<?> saveContact(@Valid @RequestBody Contact contact) {
+		ResponseEntity<?> response = null;
+		Map<String, String> errors = validator.validation(contact);
+		if (errors.isEmpty()) {
+			String message = service.upsert(contact);
+			response = ResponseEntity.ok(message);
+		} else {
+			response = new ResponseEntity<Map<String, String>>(errors, HttpStatus.BAD_REQUEST);
+		}
 		return response;
 	}
-	
+
 	@GetMapping("/contact")
-	public ResponseEntity<List<Contact>> getAllContact(){
-		ResponseEntity<List<Contact>> response=null;
-			List<Contact> list=service.getAllContact();
-			response=ResponseEntity.ok(list);	
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
+	public ResponseEntity<List<Contact>> getAllContact() {
+		ResponseEntity<List<Contact>> response = null;
+		List<Contact> list = service.getAllContact();
+		response = ResponseEntity.ok(list);
 		return response;
 	}
-	
+
 	@GetMapping("/contact/{id}")
-	public ResponseEntity<Contact> getContactById(@PathVariable Integer id){
-		ResponseEntity<Contact> response=null;
+	 @PreAuthorize("hasRole('MODERATOR')")
+	public ResponseEntity<Contact> getContactById(@PathVariable Integer id) {
+		ResponseEntity<Contact> response = null;
 		try {
-			Contact contact=service.getOneContacById(id);
-			response=ResponseEntity.ok(contact);
-		} 
-		catch (ContactNotFoundException e) {
+			Contact contact = service.getOneContacById(id);
+			response = ResponseEntity.ok(contact);
+		} catch (ContactNotFoundException e) {
 			e.printStackTrace();
 			throw e;
 		}
 		return response;
 	}
-	
+
 	@DeleteMapping("/contact/{id}")
-	public ResponseEntity<String> deleteContact(@PathVariable Integer id){
-		ResponseEntity<String> response =null;
+	 @PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<String> deleteContact(@PathVariable Integer id) {
+		ResponseEntity<String> response = null;
 		try {
-			String status=service.deleteContactById(id);
-			response=ResponseEntity.ok(status);
-		} 
-		catch (ContactNotFoundException e) {
+			String status = service.deleteContactById(id);
+			response = ResponseEntity.ok(status);
+		} catch (ContactNotFoundException e) {
 			e.printStackTrace();
 			throw e;
 		}
